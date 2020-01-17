f86a09cc2e4ee7dc9d90039c9ac3ffc1e4ce953f
Muzil9 <1154120709@qq.com>
2020-01-17 17:52:32 +0800

    ...

---
+++ src/main/java/com/jiujiu/xxgame/XXGameServer.java
@@ -0,0 +1,18 @@
+package com.jiujiu.xxgame;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@MapperScan("com.jiujiu.xxgame.mapper")
+public class XXGameServer {
+
+	public static void main(String[] args) {
+        SpringApplication.run(XXGameServer.class, args);
+	}
+
+}
---
+++ src/main/java/com/jiujiu/xxgame/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.jiujiu.xxgame.controller;
+
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+import com.jiujiu.xxgame.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+	@RequestMapping("/user/index")
+    public List<User> getIndexPage(ModelMap model){
+        List<User> list=new ArrayList<>();
+        list.add(new User("GHSIUDIU276","barea"));
+        model.put("list", list);
+        return list;
+    }
+	
+	@PostMapping("/user/login")
+    public List<User> login(@RequestParam String token, @RequestParam String uid){
+		List<User> list=new ArrayList<>();
+        System.out.print("你好 home ");
+        list.add(new User("GHSIUDIU276","barea"));
+        return list;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("ssssssssssssss");
+    }
+}
